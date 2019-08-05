f5803c0c13166440b6fc0bff00911e0814f5f3c7
Mark <mrk.rosenberg@gmail.com>
2019-08-05 10:05:12 -0600

    sets up item service

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
+// Firestore
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  itemsCollection: AngularFirestoreCollection;
+
+  constructor(public firestore: AngularFirestore) { }
 }
