f707ea1c59dc40f8dbc425c0cbdfb21eebbb8bf2
Mark <mrk.rosenberg@gmail.com>
2019-08-05 10:08:31 -0600

    imports item interface into item service

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -3,12 +3,15 @@ import { Injectable } from '@angular/core';
 // Firestore
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 
+// Interface
+import { Item } from '../models/item';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  itemsCollection: AngularFirestoreCollection;
+  itemsCollection: AngularFirestoreCollection<Item>;
 
   constructor(public firestore: AngularFirestore) { }
 }
